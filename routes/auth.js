acc638228947c98c9369acb417f9b48fee01e3a2
Julie K <julieskim160@gmail.com>
2018-04-03 12:10:08 -0700

    auth.js imports

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,21 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const {JWT_SECRET, JWT_EXPIRY} = require('../config');
+
 
 const options = {session: false, failWithError: true};
 
 const localAuth = passport.authenticate('local', options);
+
+function createAuthToken (user) {
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
+
 
 router.post('/login', localAuth, function (req, res) {
   return res.json(req.user);
