e03796435264f3ebb72b3f32bd8f9dc7cb0a251e
Julie K <julieskim160@gmail.com>
2018-04-04 08:47:40 -0700

    require seedUsers

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -10,6 +10,7 @@ const User = require('../models/user');
 
 const seedNotes = require('../db/seed/notes');
 const seedFolders = require('../db/seed/folders');
+const seedUsers = require('../db/seed/users');
 
 mongoose.connect(MONGODB_URI)
   .then(() => mongoose.connection.db.dropDatabase())
@@ -20,7 +21,7 @@ mongoose.connect(MONGODB_URI)
       Folder.createIndexes(),
       Tag.insertMany(),
       Tag.createIndexes(),
-      User.insertMany(),
+      User.insertMany(seedUsers),
       User.createIndexes()
     ]);
   })
